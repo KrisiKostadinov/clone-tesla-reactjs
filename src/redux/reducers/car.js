5d03fd0cab1e7d9023bba1d0dcf6f3114014a7d6
krisikostadinov <krisi.199898@gmail.com>
2022-12-09 23:51:31 +0200

    Updated

--- src/redux/reducers/car.js
+++ src/redux/reducers/car.js
@@ -4,14 +4,14 @@ const initialState = {
     cars: ['Model 3', 'Model Y', 'Model S', 'Model X'],
     sections: [
         {
-            bgImage: 'src/assets/Model-3.jpg',
+            bgImage: 'assets/Model-3.jpg',
             title: 'Model 3',
             subtitle: 'Schedule a Test Drive',
             leftButton: 'Custom Order',
             rightButton: 'Existing Enventory',
         },
         {
-            bgImage: 'src/assets/Model-Y.jpg',
+            bgImage: 'assets/Model-Y.jpg',
             title: 'Model Y',
             subtitle: 'Schedule a Test Drive',
             leftButton: 'Custom Order',
@@ -25,14 +25,14 @@ const initialState = {
             rightButton: 'Existing Enventory',
         },
         {
-            bgImage: 'src/assets/Model-X.jpg',
+            bgImage: 'assets/Model-X.jpg',
             title: 'Model X',
             subtitle: 'Schedule a Test Drive',
             leftButton: 'Custom Order',
             rightButton: 'Existing Enventory',
         },
         {
-            bgImage: 'src/assets/Accsessoary.jpg',
+            bgImage: 'assets/Accsessoary.jpg',
             title: 'Accessories',
             leftButton: 'Shop Now',
             accessoriesSection: true,
